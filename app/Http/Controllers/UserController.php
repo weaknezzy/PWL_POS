65fc447714a450aba8f564a6650c55ef4c899051
Yoga Pradana <yogapradana5666@gmail.com>
2024-03-04 15:56:09 +0700

    Pratikum 6 - Implementasi Eloquent ORM

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\UserModel;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+
+class UserController extends Controller
+{
+    public function index()
+    {
+        // tambah data user dengan Eloquent Model
+        $data = [
+            'username' => 'customer-1',
+            'nama' => 'Pelanggan',
+            'password' => Hash::make('12345'),
+            'level_id' => 5
+        ];
+        UserModel::where('username','customer-1')->update($data); // tambahkan data ke table m_users
+
+        //coba akses model UserModel
+        $user = UserModel::all(); //ambil semua data dari table m_users
+        return view('user',['data' => $user]);
+    }
+}
