6b74b976821a82c2c748865b12e85eef0b33c34a
Yoga Pradana <yogapradana5666@gmail.com>
2024-03-05 11:20:24 +0700

    JB4_Pratikum - 2.1

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -34,15 +34,22 @@ class UserController extends Controller
         // $user = UserModel::all();
         // return view ('user', ['data' => $user]);
 
-        $data = [
-            'level_id' => 2,
-            'username' => 'manager_tiga',
-            'nama' => 'Manager 3',
-            'password' => Hash::make('12345')
-        ];
-        UserModel::create($data);
+        // $data = [
+        //     'level_id' => 2,
+        //     'username' => 'manager_tiga',
+        //     'nama' => 'Manager 3',
+        //     'password' => Hash::make('12345')
+        // ];
+        // UserModel::create($data);
 
-        $user = UserModel::all();
-        return view('user', ['data' => $user]);
+        // $user = UserModel::firstWhere('level_id', 1);
+        // return view('user', ['data' => $user]);
+
+        $user = UserModel::findOr(20, ['username', 'nama'], function () { 
+            abort(404);
+        });
+
+        return view ('user',['data' => $user]);
+
     }
 }
