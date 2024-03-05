0f826f7ee2c87321f563eb6e5afbc4b2587a225d
Yoga Pradana <yogapradana5666@gmail.com>
2024-03-05 11:39:44 +0700

    JB4_Pratikum 2.3 - Retreiving Aggregrates

--- resources/views/user.blade.php
+++ resources/views/user.blade.php
@@ -7,22 +7,23 @@
         <h1> Data User </h1>
         <table border="1" cellpadding="2" cellspacing ="0">
             <tr>
-                <td>ID</td>
-                <tb>Username</tb>
-                <tb>Nama</tb>
+                {{-- <td>ID</td> --}}
+                {{-- <tb>Username</tb>
+                <tb>Nama</tb> --}}
+                <td> Jumlah Pengguna </td>
             </tr>
-            @foreach ($data as $d)
+            {{-- @foreach ($data as $d) --}}
             <tr>
                 {{-- <td>{{ $d -> user_id}}</td>
                 <td>{{ $d -> username}}</td>
                 <td>{{ $d -> nama}}</td>
                 <td>{{ $d -> level_id}}</td> --}}
-                <td>{{ $data->user_id}}</td>
+                {{-- <td>{{ $data->user_id}}</td>
                 <td>{{ $data->username}}</td>
-                <td>{{ $data->nama}}</td>
-                <td>{{ $data->level_id}}</td>
+                <td>{{ $data->nama}}</td> --}}
+                <td>{{ $data}}</td> 
             </tr>       
-            @endforeach
+            {{-- @endforeach --}}
         </table>
     </body>
 </html>  
