e5121fae233b6064d108250d430d90714143afa6
Yoga Pradana <yogapradana5666@gmail.com>
2024-03-07 14:47:16 +0700

    JB4_Pratikum 2.5 - Attribute Changes 1

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -60,7 +60,7 @@ class UserController extends Controller
         // $user = UserModel::where('level_id', 2)->count();
         // // dd($user);
         // return view('user', ['data' => $user]);
-
+//Pratikum 2.4 - Retreiving or Creating Models
         // $user = UserModel::firstOrCreate(
         //     [
         //         'username' => 'manager22',
@@ -71,15 +71,59 @@ class UserController extends Controller
         // );
         // return view('user', ['data' => $user]);
 
-        $user = UserModel::firstOrNew(
-            [
-                'username' => 'manager33',
-                'nama' => 'Manager Tiga Tiga',
-                'password' => Hash::make('12345'),
-                'level_id' => 2
-            ],
-        );
-        $user->save();
-        return view('user', ['data' => $user]);
+        // $user = UserModel::firstOrNew(
+        //     [
+        //         'username' => 'manager33',
+        //         'nama' => 'Manager Tiga Tiga',
+        //         'password' => Hash::make('12345'),
+        //         'level_id' => 2
+        //     ],
+        // );
+        // $user->save();
+        // return view('user', ['data' => $user]);
+//Pratikum 2.4 - Retreiving or Creatong Models
+//-----------------------------------------------//
+//Pratikum 2.5 - Attribute Changes
+        // $user = UserModel::create([
+        //     'username' => 'manager55',
+        //     'nama' => 'Manager55',
+        //     'password' => Hash::make('12345'),
+        //     'level_id' => 2,
+        // ]);
+
+        // $user->username = 'manager56';
+
+        // $user->isDirty(); //true
+        // $user->isDirty('username'); //true
+        // $user->isDirty('nama'); //false
+        // $user->isDirty(['nama', 'username']); //false
+
+        // $user->isClean();//false
+        // $user->isClean('username'); //false
+        // $user->isClean('nama'); //true
+        // $user->isClean(['nama', 'username']); //false
+
+        // $user->save();
+
+        // $user->isDirty(); //false
+        // $user->isClean(); //true
+        // dd($user->isDirty());
+
+        $user = UserModel::create([
+            'username' => 'manager11',
+            'nama' => 'Manager11',
+            'password' => Hash::make('12345'),
+            'level_id' => 2,
+        ]);
+        $user -> username = 'manager12';
+        $user -> save();
+
+        $user->wasChanged(); //true
+        $user->wasChanged('username'); //true
+        $user->wasChanged(['username','level_id']);//true
+        $user->wasChanged('nama');//false
+        dd($user->wasChanged)(['nama', 'username']);//true
+//Pratikum 2.5 Atribute Changes
+//-------------------------------------------------------//
     }
 }
