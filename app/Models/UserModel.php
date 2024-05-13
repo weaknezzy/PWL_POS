af825266f85a3cd9ea6b9b316f7526694a41f933
Yoga Pradana <yogapradana5666@gmail.com>
2024-05-13 19:23:52 +0700

    JB11_Pratikum Implementasi upload file/gambar pada UserModel

--- app/Models/UserModel.php
+++ app/Models/UserModel.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Tymon\JWTAuth\Contracts\JWTSubject;
 
@@ -21,9 +22,23 @@ class UserModel extends Authenticatable implements JWTSubject
 
     protected $table = 'm_users'; // Mendefinisikan nama table yang digunakan oleh model ini
     protected $primaryKey = 'user_id'; // Mendefinisikan primary key dari tabel yang digunakan
-    protected $fillable = ['level_id','username', 'nama', 'password'];
-    public function level(): BelongsTo
+    protected $fillable = 
+    [
+        'username',
+        'nama',
+        'password',
+        'level_id',
+        'image'
+    ];
+    public function level()
     {
         return $this->belongsTo(LevelModel::class, 'level_id','level_id');
+    }
+
+    protected function image(): Attribute
+    {
+        return Attribute::make(
+            get: fn ($image) => url('/storage/posts'.$image),
+        );
     }
 }
